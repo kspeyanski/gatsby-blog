c73581627aa59924c2be433c200d1785376d6ddd
Kiril Peyanski <peyanski@progress.com>
2019-11-17 14:42:52 +0200

    05: Query data through useStaticQuery hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const resp = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        nodes {
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  `)
+
+  return (<Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <ul>
+      {resp.allMarkdownRemark.nodes.map((node) => (
+        <li>
+          <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
+        </li>
+      ))}
+    </ul>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  </Layout>)
+}
 
 export default IndexPage
