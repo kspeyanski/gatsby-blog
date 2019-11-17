726cf44d931655933786786e448b47406b8efb5f
Kiril Peyanski <peyanski@progress.com>
2019-11-17 14:39:19 +0200

    03. Create the first page

---
+++ src/components/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Blog = (props) => {
+    return (
+        <div
+            dangerouslySetInnerHTML={{
+                __html: props.pageContext.html
+            }}
+        />
+    )
+}
+
+export default Blog;
