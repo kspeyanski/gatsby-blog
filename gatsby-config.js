2edd462e681c27926fcf18e75b57afdbd69ebcaf
Kiril Peyanski <peyanski@progress.com>
2019-11-17 15:41:23 +0200

    08. Add styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import React from "react"
+import Image from 'gatsby-image';
 import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import '../styles/main.scss';
 
 const IndexPage = () => {
   const resp = useStaticQuery(graphql`
@@ -12,16 +15,36 @@ const IndexPage = () => {
         frontmatter {
           title
           path
+          image
+        }
+      }
+    }
+    allImageSharp {
+      nodes {
+        parent {
+          ...on File {
+            relativePath
+          }
+        }
+        fluid {
+          ...GatsbyImageSharpFluid
         }
       }
     }
   }`)
   return (<Layout>
     <SEO title="Home" />
-
-    <ul>
+    <ul className="list-unstyled">
       {resp.allMarkdownRemark.nodes.map((node) => {
-        return (<li><Link to={node.frontmatter.path}>{node.frontmatter.title}</Link></li>)
+        return (<li className="list-item" key={node.frontmatter.title}>
+          <Link className="card" to={node.frontmatter.path}>
+            <Image
+              style={{ width: 100, height: 100 }}
+              fluid={resp.allImageSharp.nodes.find(i => i.parent.relativePath === node.frontmatter.image).fluid}
+            />
+            <h5 className="card-title">{node.frontmatter.title}</h5>
+          </Link>
+        </li>)
       })}
     </ul>
   </Layout>)
